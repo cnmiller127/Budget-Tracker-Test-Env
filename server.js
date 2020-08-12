96894f87ddaa14140efaea53880abae7daa3fcd1
cnmiller127 <55497906+cnmiller127@users.noreply.github.com>
2020-08-12 12:03:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const path = require("path");
 
-
-var PORT = process.env.PORT || 3017;
+const PORT = 3000;
 
 const app = express();
 
@@ -17,16 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://cnmiller127:superBase93!@ds055525.mlab.com:55525/heroku_rsb12l8c", {useNewUrlParser: true});
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}! http://localhost:3011`);
+  console.log(`App running on port ${PORT}!`);
 });
-
